adbf858fc86914196ee9399477724c14d9c79991
bufan <1173324376@qq.com>
2020-05-04 09:27:19 +0800

    会员商品管理数据库

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from web.controllers.user.User import router_user
 from web.controllers.index import route_index
 from web.controllers.static import route_static
 from web.controllers.account.Account import router_account
+from web.controllers.member.Member import router_member
 
 
 #拦截器路由
@@ -16,3 +17,4 @@ app.register_blueprint(router_user,url_prefix="/user")
 app.register_blueprint(router_goods,url_prefix="/goods")
 app.register_blueprint(route_static,url_prefix="/static")
 app.register_blueprint(router_account,url_prefix='/account')
+app.register_blueprint(router_member,url_prefix='/member')
